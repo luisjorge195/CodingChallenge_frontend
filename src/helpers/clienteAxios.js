9d2f02164830edb94be58420d9378b778313df56
Jorge Pedroza <sebastian.tobon@corprevenir.com>
2022-10-14 19:52:36 -0500

    final

--- src/helpers/clienteAxios.js
+++ src/helpers/clienteAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: `${import.meta.env.VITE_URL_BACK}api/museum`
+    baseURL: `${import.meta.env.VITE_URL_BACK}/api/museum`
 })
 export default clienteAxios
