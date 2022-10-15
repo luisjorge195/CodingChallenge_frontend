c42a5aeb542eaeff39fd1918093e05a56eabeebc
Jorge Pedroza <sebastian.tobon@corprevenir.com>
2022-10-15 07:08:03 -0500

    temporizador ajuste

--- src/context/RegistroProvider.jsx
+++ src/context/RegistroProvider.jsx
@@ -26,6 +26,7 @@ const RegistroProvider = ({children})=>{
             setNombre(' ');
             setEmail(' ');
             setPassword(' ');
+            temporizador();
 
         } catch (error) {
             setAlerta({
